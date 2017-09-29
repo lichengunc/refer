9bda5b5ba91cca85e417a8eed79c739e0b7bf5c1
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-09-29 14:24:22 -0500

    Adapt refer code to follow PEP8 guidelines, write setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,71 @@
-"""
-This code is for making mask.so, used to visualize the segmentation of referred object.
+# -*- coding: utf-8 -*-
+"""Setup script for refer.
+
+mask.so is compiled as a Cython extension used to visualize the segmentation
+of referred object.
 All "mask" related code is copied from https://github.com/pdollar/coco.git
 """
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
+
+# Standard library imports
+import os
+import ast
+
+# Third party imports
 import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(module='refer'):
+    """Get version from text file and avoids importing the module."""
+    with open(os.path.join(HERE, module, '__init__.py'), 'r') as f:
+        data = f.read()
+    lines = data.split('\n')
+    for line in lines:
+        if line.startswith('VERSION_INFO'):
+            version_tuple = ast.literal_eval(line.split('=')[-1].strip())
+            version = '.'.join(map(str, version_tuple))
+            break
+    return version
+
 
 ext_modules = [
-            Extension(
-                'external._mask',
-                sources=['external/maskApi.c', 'external/_mask.pyx'],
-                include_dirs = [np.get_include(), 'external'],
-                extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-            )
-        ]
+    Extension(
+        'refer.external._mask',
+        sources=['refer/external/maskApi.c', 'refer/external/_mask.pyx'],
+        include_dirs=[np.get_include(), 'external'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
+
+REQUIREMENTS = ['cython', 'numpy', 'matplotlib', 'scikit-image']
 
 setup(
-    name='external',
-    packages=['external'],
-    package_dir = {'external': 'external'},
-    version='2.0',
-    ext_modules=cythonize(ext_modules)
-    )
+    name='referit',
+    version=get_version(),
+    keywords=['referit', 'segmentation', 'dataset'],
+    url='https://github.com/lichengunc/refer',
+    license='MIT',
+    author='Licheng Yu',
+    author_email='licheng@cs.unc.edu',
+    description='Python wrapper to load the ReferIt Game dataset',
+    ext_modules=cythonize(ext_modules),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
